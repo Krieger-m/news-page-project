92d0e4a0387dce2721901d938ac80edf2b186a63
Mat <matze.krieger@googlemail.com>
2025-11-28 11:26:32 +0100

    changed header title

--- src/_components/Header.tsx
+++ src/_components/Header.tsx
@@ -4,7 +4,7 @@ export function Header (){
     return(
         <>
             <div style={{padding:20, backgroundColor: '#cbc7b7', color: 'black'}}>
-                <h1>Header</h1>
+                <h1>Next News</h1>
                 <Link href={'/'}>Home</Link>
                 <span> | </span>
                 <Link href={'/news'}>News</Link>
