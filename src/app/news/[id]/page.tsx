b4d60682ee88868826e01be7a9a4a9e341213c51
Mat <matze.krieger@googlemail.com>
2025-11-28 11:10:26 +0100

    added dynamic newsId via pageParams

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -1,14 +1,27 @@
 import Link from "next/link";
 import styles from "../../page.module.css";
 
-export default function NewsIdPage() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>Individual News Page</h1>
-        <br/>
-        <Link href={'/'}>Back home ...</Link>
-      </main>
-    </div>
-  );
+interface NewsIdPageProps{
+    params: {
+        id:Promise<string>;
+    }
+}
+
+export default async function NewsIdPage({ params }: NewsIdPageProps) {
+    const newsId = (await params).id;
+  return ( 
+    <div className={styles.page}> 
+      <main className={styles.main}> 
+        <h1>Individual News Page</h1> 
+        <br/> 
+        <p>News article ID: {newsId}</p>
+        <br/> 
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <br/> 
+        <Link href={'/news'}>{'<- back'}</Link> 
+      </main> 
+    </div> 
+  ); 
 }
--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -7,11 +7,11 @@ export default function NewsPage() {
       <main className={styles.main}>
         <h1>News Page</h1>
         <br/>
-        <Link href={'/news/11'}>News Article 1</Link>
+        <Link href={'/news/1'}>News Article 1</Link>
         <br/>
-        <Link href={'/news/12'}>News Article 2</Link>
+        <Link href={'/news/2'}>News Article 2</Link>
         <br/>
-        <Link href={'/news/13'}>News Article 3</Link>
+        <Link href={'/news/3'}>News Article 3</Link>
       </main>
     </div>
   );
